ad6248d5137bb87f7926992b9a192a1833342939
Diego <diegolara93345@gmail.com>
2024-01-02 14:09:02 -0600

    day1 advent of code

---
+++ cmd/day1.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var re = regexp.MustCompile("[0-9]")
+
+func addCount(text []string) int {
+	if len(text) > 1 {
+		for i := range text {
+			arr := re.FindAllString(text[i], -1)
+			twoNumbers := arr[0] + arr[len(arr)-1]
+			fmt.Println(twoNumbers)
+			convInt, err := strconv.Atoi(twoNumbers)
+			if err != nil {
+				panic(err)
+			}
+			return convInt + addCount(text[i+1:])
+		}
+	}
+	return 0
+}
+
+func main() {
+	content, error := os.ReadFile("d:/go/advent-of-code/day1/static/input.txt")
+	contentString := string(content)
+	splitString := strings.Split(contentString, "\n")
+	fmt.Println((addCount(splitString)))
+	if error != nil {
+		fmt.Println("Error", error)
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module d/go/advent-of-code/day1
+
+go 1.21.5
